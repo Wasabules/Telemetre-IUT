d6cf596539f0c75d8f1b1049f902050a19c86b7b
Geoffrey Lecoq <39313803+Wasabules@users.noreply.github.com>
2021-02-13 19:05:41 +0100

    Mise en place du module ultra son + optimisation du code

--- src/main.cpp
+++ src/main.cpp
@@ -1,71 +1,77 @@
 #include <Arduino.h>
 #include <util/delay.h>
   
-byte chiffres[10] = {0b0111111,0b0000110,0b0011011,0b1001111,0b0100110,0b1101101,0b1111101,0b0000111,0b1111111,0b1101111};
-
+byte chiffres[10] = {0b0111111,0b0000110,0b0011011,0b1001111,0b0100110,0b1101101,0b1111101,0b0000111,0b1111111,0b1101111};    // Table de transcodage des chiffres sur les digits
+byte selectDigit[4] = {0b000111, 0b001011, 0b001101, 0b001110};                                                               // Table de transcodage du digit à allumer/sélectionner
 int nombre;
-int digitOn = 1;
+int digitOn = 0;
 
-void afficheur(int digit, int numero){
-  if(digit == 1){         // Unité
-      PORTB = 0b000111;
-      PORTC = chiffres[numero];
-  }else if(digit == 2){   // Dizaine
-      PORTB = 0b001011;
-      PORTC = chiffres[numero];  
-  }else if(digit == 3){   // Centaine
-      PORTB = 0b001101;
-      PORTC = chiffres[numero];
-  }else if(digit == 4){   // Millier
-      PORTB = 0b001110;
-      PORTC = chiffres[numero];
-  }
+int distance(){
+  PORTD |= (1 << PD2);
+  delayMicroseconds(10);
+  PORTD &=~ (1 << PD2);
+  int lecture_echo = pulseIn(PD3, HIGH);
+  return (lecture_echo/58);
 }
 
-ISR(TIMER1_OVF_vect){
+void afficheur(int digit, int numero){    // 
+  PORTB = selectDigit[digit];
+  PORTC = chiffres[numero];
+}
+
+ISR(TIMER1_OVF_vect){       // Permet l'affichage des digits de façon fluide et qui n'influe pas sur la vitesse du programme (du moins, c'est négligeable)
   switch(digitOn)
   {
-    case 4:
-      afficheur(4, (nombre /1000)%10);
-      digitOn = 3;
-      break;
     case 3:
-      afficheur(3, (nombre /100)%10);
+      afficheur(3, (nombre /1000)%10);
       digitOn = 2;
       break;
     case 2:
-      afficheur(2, (nombre /10)%10);
+      afficheur(2, (nombre /100)%10);
       digitOn = 1;
       break;
     case 1:
-      afficheur(1, (nombre)%10);
-      digitOn = 4;
+      afficheur(1, (nombre /10)%10);
+      digitOn = 0;
+      break;
+    case 0:
+      afficheur(0, (nombre)%10);
+      digitOn = 3;
       break;
   }
 }
 
+int tempsEnvoi, tempsSignal;
+
+void setupTimer7digit(){
+  TIMSK1 |= (1 << TOIE1);                 // Activation de l'interruption par overflow du Timer 1
+  TCNT1 = 0;                              // Mise à 0 du compteur
+  TCCR1B = (1 << CS11) | (1 << CS10);     // Selection du prescaler du timer1 (/64), celui-ci changera directement la fréquence de rafraichissement des digits
+  sei();                                  // Autorisation des interruptions
+}
+
+void setupPort(){
+  // Setup du 7 digit
+  DDRC = 0b1111111;     // Mise en sortie du port C pour les chiffres des digitis
+  DDRB = 0b111111;      // Mise en sortie du port B pour la selection des digits 
+  PORTC = 0b0000000;    // Mise à 0 des sorties
+  PORTB = 0b000000;     // Mise à 0 des sorties
+
+  // Setup du module ultra son
+  DDRD |= (1 << PD2);   // Mise en sortie PD2 pour le "Trigger" du module ultra son
+  DDRD &=~ (1 << PD3);  // Mise en entrée PD3 pour le "Echo" du module ultra son
+}
+
 void setup() {
-
   Serial.begin(9600);
-
-  DDRC = 0b1111111;
-  DDRB = 0b111111;
-  PORTC = 0b0000000;
-  PORTB = 0b000000;
-
-  TIMSK1 |= (1 << TOIE1);
-  TCNT1 = 0;
-  TCCR1B = (1 << CS11) | (1 << CS10);
-  
-  sei();
+  setupPort();
+  setupTimer7digit();
 
   Serial.println("Démarrage");
 }
 
 void loop() {
-  for(int i = 0; i<10000; i++){
-    nombre = i;
-    _delay_ms(10);
-    Serial.println(nombre);
-  }
+  nombre = distance();
+  Serial.print("Distance : ");
+  Serial.println(nombre);
 }
