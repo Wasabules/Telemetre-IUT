515e7399e4dc6779f758a811c47f0a0a67cd3875
Geoffrey Lecoq <39313803+Wasabules@users.noreply.github.com>
2021-06-06 02:12:11 +0200

    Défranglisation du code + Suppression des variables inutiles

--- src/main.cpp
+++ src/main.cpp
@@ -10,29 +10,24 @@
 #define Dig4 PB3
 #define trigger PB5
 #define echo PB4
-#define bouton PB6
+#define button PB6
 
-char chiffres[10] = {0b00111111, 0b00000110, 0b01011011, 0b01001111, 0b01100110, 0b01101101, 0b01111101, 0b00000111, 0b01111111, 0b01101111}; // Table de transcodage des chiffres sur les digits
+char numerals[10] = {0b00111111, 0b00000110, 0b01011011, 0b01001111, 0b01100110, 0b01101101, 0b01111101, 0b00000111, 0b01111111, 0b01101111}; // Table de transcodage des chiffres sur les digits
 char selectDigit[4] = {0b01000111, 0b01001011, 0b01001101, 0b01001110};                                                                       // Table de transcodage du digit à allumer/sélectionner
 char OUT[3]= {0b01111000,0b00111110,0b00111111};
 char outDigit[4] = {0b01000111, 0b01001011, 0b01001101, 0b01001111};   
 
-int unsigned nombre;
+int unsigned number;
 unsigned char digitOn = 0;
-int unsigned timer0_overflow_count;
-
-int unsigned receptionSignal, tempsEmis, finSignal;
-int unsigned centimetre = 0;
-int unsigned moyenneDistance, sommeDistance;
-int unsigned clic = 0;
-bool numberSet = false;
+int unsigned timer_distance = 0;
+int unsigned distanceAvg, distanceSum;
 int counter;
 
 
 int distance()
 { // Retourne la valeur de la distance en cm (2 chiffre après virgule)
 
-  centimetre=0;
+  timer_distance=0;
   PORTB |= (1 << trigger);
   _delay_us(100);
   PORTB &=~ (1 << trigger);
@@ -43,22 +38,21 @@ int distance()
   loop_until_bit_is_clear(PINB, echo);
 
   TIMSK &= ~(1 << OCIE0A);
-  return centimetre;
+  return timer_distance;
 
   // En divisant par 58, on retourne la valeur en centimètre
 
 }
 
-void afficheur(unsigned char digit, unsigned int numero)
+void display(unsigned char digit, unsigned int n)
 { // Affiche un numéro sur un digit
-  if(nombre != 9999){
-    PORTA = chiffres[numero];
+  if(number != 9999){
+    PORTA = numerals[n];
     PORTB = selectDigit[digit];
   }else{
     PORTA = OUT[digit];
     PORTB = outDigit[digit];
   }
-  
 }
 
 ISR(TIMER1_OVF_vect)
@@ -67,19 +61,19 @@ ISR(TIMER1_OVF_vect)
   switch (digitOn)
   {
   case 3:
-    afficheur(3, (nombre / 1000) % 10);
+    display(3, (number / 1000) % 10);
     digitOn = 2;
     break;
   case 2:
-    afficheur(2, (nombre / 100) % 10);
+    display(2, (number / 100) % 10);
     digitOn = 1;
     break;
   case 1:
-    afficheur(1, (nombre / 10) % 10);
+    display(1, (number / 10) % 10);
     digitOn = 0;
     break;
   case 0:
-    afficheur(0, (nombre) % 10);
+    display(0, (number) % 10);
     digitOn = 3;
     break;
   }
@@ -90,13 +84,12 @@ void setupTimer1_7digit()
   TIMSK |= (1 << TOIE1);              // Activation de l'interruption par overflow du Timer 1
   TCNT1 = 0;                          // Mise à 0 du compteur
   TCCR1B = (1 << CS12) | (1 << CS11); // Selection du prescaler du timer1 (/64), celui-ci changera directement la fréquence de rafraichissement des digits
-
 }
 
 
 ISR(TIMER0_COMPA_vect)
 {
-  centimetre++;
+  timer_distance++;
 }
 
 void setupTimer0_duree()
@@ -118,15 +111,15 @@ void setupPort()
 
   DDRB |= (1 << trigger); // Mise en sortie PD2 pour le "Trigger" du module ultra son
   DDRB &= ~(1 << echo);   // Mise en entrée PD3 pour le "Echo" du module ultra son
-  DDRB &=~ (1<< bouton);
-  PORTB |= (1<< bouton);
+  DDRB &=~ (1<< button);
+  PORTB |= (1<< button);
 }
 
 void setupInterrupt(){
   GIMSK |= 1 << INT0;
 }
 
-void eteindre(){
+void shutdown(){
   DDRB = 0;
   DDRA = 0;
   sleep_mode();
@@ -151,19 +144,19 @@ int main()
         loop_until_bit_is_clear(PORTB,PB6);
       }
       // Calcul de somme de distance pour augmenter la fidélité du résultat
-      sommeDistance = 0;
+      distanceSum = 0;
       for(int i = 0; i<10;i++){
-        sommeDistance += distance();
+        distanceSum += distance();
       }
-      moyenneDistance = sommeDistance/10;
-      if(moyenneDistance >  1000){ // Si le nombre est suppérieur à sa limite de fonctionnement
-        nombre = 9999;
+      distanceAvg = distanceSum/10;
+      if(distanceAvg >  1500){ // Si le nombre est suppérieur à sa limite de fonctionnement (Après 1m50 il a du mal)
+        number = 9999;
       }else{
-        nombre = moyenneDistance; // "nombre" est directement afficher sur l'écran et affichera la moyenne
+        number = distanceAvg; // "nombre" est directement afficher sur l'écran et affichera la moyenne
       }
       
     }else{
-      eteindre();   // On éxecute des opérations pour éteindre
+      shutdown();   // On éxecute des opérations pour éteindre
     }
     counter++;    // On incrémente notre compteur
     _delay_ms(100);
